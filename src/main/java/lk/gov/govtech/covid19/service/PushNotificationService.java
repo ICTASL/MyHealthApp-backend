6bb185d8245463945c80c408736732fd523f012f
Sankha Lakshan Karunasekara <sankhadlk@gmail.com>
2020-03-15 21:05:34 +0530

    FCM backend changes

---
+++ src/main/java/lk/gov/govtech/covid19/service/PushNotificationService.java
@@ -0,0 +1,81 @@
+package lk.gov.govtech.covid19.service;
+
+import lk.gov.govtech.covid19.firebase.FCMService;
+import lk.gov.govtech.covid19.model.PushNotificationRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class PushNotificationService {
+
+    @Value("#{${app.notifications-defaults}}")
+    private Map<String, String> defaults;
+
+    private Logger logger = LoggerFactory.getLogger(PushNotificationService.class);
+    private FCMService fcmService;
+
+    public PushNotificationService(FCMService fcmService) {
+        this.fcmService = fcmService;
+    }
+
+    @Scheduled(initialDelay = 60000, fixedDelay = 60000)
+    public void sendSamplePushNotification() {
+        try {
+            fcmService.sendMessageWithoutData(getSamplePushNotificationRequest());
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void sendPushNotification(PushNotificationRequest request) {
+        try {
+            fcmService.sendMessage(getSamplePayloadData(), request);
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public void sendPushNotificationWithoutData(PushNotificationRequest request) {
+        try {
+            fcmService.sendMessageWithoutData(request);
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+
+    public void sendPushNotificationToToken(PushNotificationRequest request) {
+        try {
+            fcmService.sendMessageToToken(request);
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+
+    private Map<String, String> getSamplePayloadData() {
+        Map<String, String> pushData = new HashMap<>();
+        pushData.put("messageId", defaults.get("payloadMessageId"));
+        logger.info("payloadMessageId:" + defaults.get("payloadMessageId"));
+        pushData.put("text", defaults.get("payloadData") + " " + LocalDateTime.now());
+        return pushData;
+    }
+
+
+    private PushNotificationRequest getSamplePushNotificationRequest() {
+        PushNotificationRequest request = new PushNotificationRequest(defaults.get("title"),
+                defaults.get("message"),
+                defaults.get("topic"));
+        return request;
+    }
+
+
+}
