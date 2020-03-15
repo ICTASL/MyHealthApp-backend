5c5a7b7ba0f04e24ce66bd7029106ca44dd17422
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-03-15 20:12:41 +0530

    basic frontend related files added

--- src/main/java/lk/gov/govtech/covid19/controller/NewsNotificationController.java
+++ src/main/java/lk/gov/govtech/covid19/controller/NewsNotificationController.java
@@ -1,13 +1,20 @@
 package lk.gov.govtech.covid19.controller;
 
 import lk.gov.govtech.covid19.util.Constants;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller class for all health promotion bureau related APIs. i.e: from web portal
  */
 @RestController
-@RequestMapping(value = Constants.HPB_API_CONTEXT)
-public class HPBNotificationController {
+@RequestMapping(value = Constants.NEWS_API_CONTEXT)
+public class NewsNotificationController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(Model model) {
+        return "newsPortal";
+    }
 }
