31cd378cea5ec77f08a0ceb5f08fdebfc93906cd
Godwin <godwinamila@gmail.com>
2020-03-15 15:45:56 +0000

    Add Configurations

---
+++ src/main/java/lk/gov/govtech/covid19/config/DHISConfiguration.java
@@ -0,0 +1,47 @@
+package lk.gov.govtech.covid19.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+@Data
+@Component
+public class DHISConfiguration {
+
+    private String url;
+    private String username;
+    private String password;
+
+    public String getUrl() {
+
+        return url;
+    }
+
+    public void setUrl(String url) {
+
+        this.url = url;
+    }
+
+    public String getUsername() {
+
+        return username;
+    }
+
+    public void setUsername(String username) {
+
+        this.username = username;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    public void setPassword(String password) {
+
+        this.password = password;
+    }
+}
