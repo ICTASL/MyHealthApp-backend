a00652a2dcbffc13211c51283f9cce9c6b7417f7
keshan <stkeshansandeepa@gmail.com>
2020-03-16 10:03:11 +0530

    Fixed the Issue View Not Returning

--- src/main/java/lk/gov/govtech/covid19/controller/CaseNotificationController.java
+++ src/main/java/lk/gov/govtech/covid19/controller/CaseNotificationController.java
@@ -1,6 +1,7 @@
 package lk.gov.govtech.covid19.controller;
 
 import lk.gov.govtech.covid19.util.Constants;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Controller class for all epid related APIs. i.e: from web portal
  */
-@RestController
+@Controller
 @RequestMapping(value = Constants.CASE_API_CONTEXT)
 public class CaseNotificationController {
 
