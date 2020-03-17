cfe8a2620fc67f47b7aacf583094a9b2c42cdf8f
azhamn <azhamnaphiel@gmail.com>
2020-03-17 16:39:42 +0530

    changed timezone to asia/colombo

--- src/main/java/lk/gov/govtech/covid19/Covid19Application.java
+++ src/main/java/lk/gov/govtech/covid19/Covid19Application.java
@@ -1,11 +1,24 @@
 package lk.gov.govtech.covid19;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Slf4j
 @SpringBootApplication
 public class Covid19Application {
+
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Colombo"));   // It will set UTC timezone
+        log.info("Spring boot application running in Asia/Colombo timezone :"+ new Date());
+
+    }
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Covid19Application.class);
