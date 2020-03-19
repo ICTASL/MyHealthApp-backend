45c298b8927d082224b6a2cb1ca851f9219ebf2a
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-03-19 14:36:41 +0530

    case ui map-api-key to be added from app.yml

--- src/main/java/lk/gov/govtech/covid19/controller/WebPortalController.java
+++ src/main/java/lk/gov/govtech/covid19/controller/WebPortalController.java
@@ -1,6 +1,9 @@
 package lk.gov.govtech.covid19.controller;
 
+import lk.gov.govtech.covid19.config.GAPIConfigurations;
+import lk.gov.govtech.covid19.config.PortalUserConfiguration;
 import lk.gov.govtech.covid19.util.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,9 @@ import static lk.gov.govtech.covid19.util.Constants.*;
 @RequestMapping(value = Constants.PORTAL_API_CONTEXT)
 public class WebPortalController {
 
+    @Autowired
+    GAPIConfigurations gAPIConfig;
+
     @GetMapping
     public String login(Model model) {
         return "login";
@@ -27,6 +33,7 @@ public class WebPortalController {
 
     @GetMapping(CASES_PATH)
     public String cases(Model model) {
+        model.addAttribute("mapKey", gAPIConfig.getMapKey());
         return "casePortal";
     }
 }
