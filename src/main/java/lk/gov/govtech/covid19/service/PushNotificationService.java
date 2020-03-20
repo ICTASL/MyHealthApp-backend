33870d3a7f82e29f74ee336d603b877622d1f691
azhamn <azhamnaphiel@gmail.com>
2020-03-20 16:19:19 +0530

    removing unncessary firebase config properties

--- src/main/java/lk/gov/govtech/covid19/service/PushNotificationService.java
+++ src/main/java/lk/gov/govtech/covid19/service/PushNotificationService.java
@@ -16,46 +16,11 @@ import java.util.concurrent.ExecutionException;
 @Service
 public class PushNotificationService {
 
-	@Value("${firebase.notifications.topic}")
-	private String topic;
-
-	@Value("${firebase.notifications.title}")
-	private String title;
-
-	@Value("${firebase.notifications.message}")
-	private String message;
-
-	@Value("${firebase.notifications.token}")
-	private String token;
-
-	@Value("${firebase.notifications.payloadMessageId}")
-	private String payloadMessageId;
-
-	@Value("${firebase.notifications.payloadData}")
-	private String payloadData;
-
 	private Logger logger = LoggerFactory.getLogger(PushNotificationService.class);
 	private FCMService fcmService;
 
 	public PushNotificationService(FCMService fcmService) {
 		this.fcmService = fcmService;
-	}
-
-	@Scheduled(initialDelay = 60000, fixedDelay = 60000)
-	public void sendSamplePushNotification() {
-		try {
-			fcmService.sendMessageWithoutData(getSamplePushNotificationRequest());
-		} catch (InterruptedException | ExecutionException e) {
-			logger.error(e.getMessage());
-		}
-	}
-
-	public void sendPushNotification(PushNotificationRequest request) {
-		try {
-			fcmService.sendMessage(getSamplePayloadData(), request);
-		} catch (InterruptedException | ExecutionException e) {
-			logger.error(e.getMessage());
-		}
 	}
 
 	public void sendPushNotificationWithoutData(PushNotificationRequest request) {
@@ -82,17 +47,5 @@ public class PushNotificationService {
 		}
 	}
 
-	private Map<String, String> getSamplePayloadData() {
-		Map<String, String> pushData = new HashMap<>();
-		pushData.put("messageId", payloadMessageId);
-		logger.info("payloadMessageId:" + payloadMessageId);
-		pushData.put("text", payloadData + " " + LocalDateTime.now());
-		return pushData;
-	}
-
-	private PushNotificationRequest getSamplePushNotificationRequest() {
-		PushNotificationRequest request = new PushNotificationRequest(title, message, topic);
-		return request;
-	}
 
 }
