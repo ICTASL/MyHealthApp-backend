555e886d112cc8c29797c1659eb286fc08577091
azhamn <azhamnaphiel@gmail.com>
2020-03-21 02:46:17 +0530

    changes to distribution packaging

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-nohup java -jar ./covid19.jar&
+nohup java -jar ./@project.artifactId@-@project.version@.jar&
 
 if [ $? -eq 0 ]
 then
    echo "Started"
 fi
+
+tail -150f logs/`ls -alh logs| head -n 4 | tail -n 1 | awk '{print $9}'`
