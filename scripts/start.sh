52837bd3aa2dcf08442ecbf80a1a1d26607ab774
azhamn <azhamnaphiel@gmail.com>
2020-03-21 09:45:11 +0530

    removing maven resource plugin

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-nohup java -jar ./@project.artifactId@-@project.version@.jar&
+nohup java -jar ./covid19.jar&
 
 if [ $? -eq 0 ]
 then
    echo "Started"
 fi
+
+sleep 2
 
 tail -150f logs/`ls -alh logs| head -n 4 | tail -n 1 | awk '{print $9}'`
