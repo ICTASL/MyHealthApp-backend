925add58828f0ddf3fe3a57240ecb78fa42f336d
Dinidu <iamdinidu@gmail.com>
2020-03-22 08:43:13 +0530

    Initialize unit/integration tests

---
+++ src/test/java/lk/gov/govtech/covid19/Covid19ApplicationIT.java
@@ -0,0 +1,2 @@
+package lk.gov.govtech.covid19;
+
---
+++ src/test/java/lk/gov/govtech/covid19/utils/CommonUtilsTest.java
@@ -0,0 +1,9 @@
+package lk.gov.govtech.covid19.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CommonUtilsTest {
+
+}
