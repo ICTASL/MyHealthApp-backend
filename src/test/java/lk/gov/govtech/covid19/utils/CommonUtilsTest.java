6e6988beb44e1b56a85a733836ff8ed117165a96
Dinidu <iamdinidu@gmail.com>
2020-03-22 09:03:19 +0530

    initialize sample tests

--- src/test/java/lk/gov/govtech/covid19/utils/CommonUtilsTest.java
+++ src/test/java/lk/gov/govtech/covid19/utils/CommonUtilsTest.java
@@ -1,9 +1,19 @@
-package lk.gov.govtech.covid19.util;
+package lk.gov.govtech.covid19.utils;
 
 import org.junit.jupiter.api.Test;
+
+import lk.gov.govtech.covid19.util.Constants;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CommonUtilsTest {
+
+    @Test
+    void testConstants() {
+        
+        assertThat(Constants.PUSH_NOTIFICATION_MESSAGE_TYPE_ALERT).isEqualTo("alert");
+        assertThat(Constants.PUSH_NOTIFICATION_MESSAGE_TYPE_CASE).isEqualTo("case");
+ 
+    }
 
 }
