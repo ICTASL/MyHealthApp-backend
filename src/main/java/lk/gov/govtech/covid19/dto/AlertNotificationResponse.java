d8c6bb92112e179925709e39313ea0a09c6c2704
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-04-06 22:23:25 +0530

    few more changes to last commit

--- src/main/java/lk/gov/govtech/covid19/dto/AlertNotificationResponse.java
+++ src/main/java/lk/gov/govtech/covid19/dto/AlertNotificationResponse.java
@@ -6,7 +6,6 @@ import lombok.Data;
 public class AlertNotificationResponse {
     private String id;
     private String title;
-    private String subtitle;
     private String source;
     private String message;
     private String createdTime;
