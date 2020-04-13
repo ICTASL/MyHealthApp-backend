087f43ef922d3cf47b5dc18261624d3720f70e80
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-04-13 21:20:57 +0530

    Edit poms, readmes, gitignore, app.yaml. Add webconfig.

---
+++ backend/src/main/java/lk/gov/govtech/covid19/config/WebappConfiguration.java
@@ -0,0 +1,71 @@
+package lk.gov.govtech.covid19.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Slf4j
+@Configuration
+public class WebappConfiguration {
+
+
+    @Bean
+    @ConditionalOnProperty(name = "webapp.war.filename")
+    public ServletWebServerFactory servletContainer(@Value("${webapp.war.filename}") String filename,
+                                                    @Value("${webapp.war.context}") String contextPath) {
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                ((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
+            }
+
+            protected TomcatWebServer getTomcatWebServer(Tomcat tomcat) {
+                //Springboot's embedded tomcat server does not have a webapp folder by default.
+                //Thus, create.
+                new File(tomcat.getServer().getCatalinaBase(), "webapps").mkdirs();
+
+                String source = getWarPath(filename);
+                try {
+                    if (source != null) {
+                        tomcat.addWebapp(contextPath, Paths.get(source).toAbsolutePath().toString());
+                    }
+
+                } catch (Exception ex) {
+                    log.error("Unable to find admin-webapp.war file. Starting backend without webapp");
+                }
+                return super.getTomcatWebServer(tomcat);
+            }
+        };
+    }
+
+    private String getWarPath(String filename){
+        String webappTargetLocation = ".." + File.separator
+                + "admin-webapp" + File.separator
+                + "target" + File.separator
+                + filename;
+
+        if (new File(filename).exists()) {
+            //Useful when running the jar -> check within the folder where the jar is
+            return filename;
+
+        } else if (new File(webappTargetLocation).exists()) {
+            //Useful when running the mvn spring-boot:run command
+            return webappTargetLocation;
+
+        } else return null;
+    }
+}
