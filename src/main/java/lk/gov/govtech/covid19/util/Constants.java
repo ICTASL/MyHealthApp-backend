83061802fc57891b567a373f2fa094f7f01e7090
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-04-13 11:09:51 +0530

    Remove portal controller. Make auth changes.

--- src/main/java/lk/gov/govtech/covid19/util/Constants.java
+++ src/main/java/lk/gov/govtech/covid19/util/Constants.java
@@ -6,15 +6,10 @@ public class Constants {
     public static final String TF_API_CONTEXT = "/taskforce";
     //context for all calls related to dhis service integration
     public static final String DHIS_API_CONTEXT = "/dhis";
-    public static final String PORTAL_API_CONTEXT = "/portal";
     public static final String APPLICATION_API_CONTEXT = "/application";
     public static final String NOTIFICATION_API_CONTEXT = "/notification";
     public static final String DOCUMENTS_API_CONTEXT = "/documents";
     public static final String AUTH_API_CONTEXT = "/auth";
-
-    public static final String NEWS_PATH = "/news";
-    public static final String CASES_PATH = "/cases";
-    public static final String DASHBOARD_PATH = "/dashboard";
 
     public static final String PUSH_NOTIFICATION_MESSAGE_TYPE_ALERT = "alert";
     public static final String PUSH_NOTIFICATION_MESSAGE_TYPE_CASE = "case";
