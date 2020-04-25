8cd1fa8528066a5083c23be8060f4c955f717d60
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-04-25 14:49:01 +0530

    change contexts

--- portal-frontend/vue.config.js
+++ portal-frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '^/api': { 
-                target: 'http://localhost:3000', 
+                target: 'http://localhost:8000', 
                 changeOrigin: true, 
                 secure:false, 
                 pathRewrite: {'^/api': '/api'}, 
