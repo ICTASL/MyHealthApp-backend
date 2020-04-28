c8386d5b528c066ab1fdef38ece292c76cb61e43
Subhashinie Koshalya <subhashinieksw.14@cse.mrt.ac.lk>
2020-04-28 13:34:45 +0530

    update assembly, webconfig, poms, doeckerfile, frontend xmls, postman, readmes, app.yml

--- Production.Dockerfile
+++ Production.Dockerfile
@@ -1,13 +1,14 @@
 FROM maven:3.5.2-jdk-8-alpine AS build
-COPY assembly.xml /tmp/
+COPY assembly /tmp/assembly
+COPY backend /tmp/backend
+COPY portal-frontend /tmp/portal-frontend
 COPY pom.xml /tmp/
-COPY src /tmp/src/
 WORKDIR /tmp/
 RUN mvn clean install
-RUN unzip /tmp/target/covid19-1.0.0-SNAPSHOT.zip -d /tmp/target/
+RUN unzip /tmp/assembly/target/myhealth-server-1.0.0.zip -d /tmp/target/
 RUN ls -l /tmp/target
 
 FROM frolvlad/alpine-java:jre8-slim
 EXPOSE 8000
-COPY --from=build /tmp/target/covid19-1.0.0-SNAPSHOT/*.* ./
-ENTRYPOINT ["java","-jar","covid19.jar"]
+COPY --from=build /tmp/target/myhealth-server-1.0.0/*.* ./
+ENTRYPOINT ["java","-jar","myhealth-server.jar"]
--- backend/src/main/java/lk/gov/govtech/covid19/config/WebappConfiguration.java
+++ backend/src/main/java/lk/gov/govtech/covid19/config/WebappConfiguration.java
@@ -1,6 +1,8 @@
 package lk.gov.govtech.covid19.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Context;
+import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -31,7 +33,18 @@ public class WebappConfiguration {
                 String source = getWarPath(filename);
                 try {
                     if (source != null) {
-                        tomcat.addWebapp(contextPath, Paths.get(source).toAbsolutePath().toString());
+                        Context appContext = tomcat.addWebapp(contextPath, Paths.get(source).toAbsolutePath().toString());
+
+                        /* The dependancy tomcat-embed-jasper is added to serve the war without getting
+                        an "error" log, showing a FileNotFound exception for JasterServelet, by tomcat.
+                        The war is served fine even without this dependency, but there was no other way
+                        to avoid the "error" log. Adding this causes another set of "warning" logs that was
+                        looking for io/grpc/grpc-netty-shaded (either wrong location or version).
+                        These were ignored by adding "additional-tld-skip-patterns" to app.yaml for spring app,
+                        and via context.xml for portal frontend. The two lines below makes sure tomcat-embed-jasper
+                        is accessible even when running the jar not just spring-boot:run */
+                        WebappLoader loader = new WebappLoader(Thread.currentThread().getContextClassLoader());
+                        appContext.setLoader(loader);
                     }
                 } catch (Exception ex) {
                     log.error("Unable to find admin-webapp.war file. Starting backend without webapp");
